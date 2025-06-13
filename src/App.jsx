f7f821a4662537517d3f9add325ab34c554a9b70
SG-Arkar Phyo <softguide.arkarphyo@gmail.com>
2025-06-13 17:55:05 +0630

    Add index

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,32 @@ import {
   Experience,
   Feedbacks,
   Hero,
-  Loader,
   Navbar,
   Tech,
   Works,
+  StarsCanvas,
 } from "./components";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>{/* Your routes will go here */}</BrowserRouter>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
